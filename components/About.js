3494436ec0d3eeb1c87d3723ac58f06447ff0429
nuru ibrahim <nuruibrahim11111@gmail.com>
2023-02-23 00:06:49 -0800

    our restaurant story

--- components/About.js
+++ components/About.js
@@ -10,12 +10,8 @@ function About() {
         <div className={Style.aboutComponentTextParent}>
            <div className={Style.aboutComponentTextTitle}>our story</div>
            <div className={Style.aboutComponentText}>
-            llllllllllllllllllfffffffffffffffffffffffffffffffffffffffffffffffffffff
-           kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkccccccccccccccccccccccc
-           fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-           jjjjjjjjjjjjkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
-           nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-           mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+           our opened on Thanksgiving Day 1990. Chef / Owner Ron Silver began baking pies and selling them to restaurants and his neighbors out of a small kitchen at the corner of Hudson and North Moore St. in Tribeca. Today, NYC’s beloved restaurant and pie shop celebrates 27 years of classic, made from scratch American cooking.
+           Rather than calling itself a “New York institution” or using the word "reliable," Bubby’s uses its headline to make a specific claim — ”Served every day since 1990” — that demonstrates those ideas. This captures more attention and paints a richer, more vivid picture of its history and role in the community. The rest of the description supports this by adding details to the story of its founding and explaining that even three decades later, its scratch approach to cooking remains the same.
            </div>
             </div>
       </div>
