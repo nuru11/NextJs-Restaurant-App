c7af267e33969509cb94ec18214172a65fde4725
nuru ibrahim <nuruibrahim11111@gmail.com>
2023-02-27 23:28:52 -0800

    p

---
+++ pages/profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from "next/link"
+
+function Profile() {
+  return (
+    <div>
+       <div>this page hasn`t build yet!</div>
+       <div>go <Link href="/">home</Link></div>
+    </div>
+  )
+}
+
+export default Profile
