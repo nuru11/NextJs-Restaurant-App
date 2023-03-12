0298aaf538942b4c51cadc99452c2659f05e65c7
nuru ibrahim <nuruibrahim11111@gmail.com>
2023-03-12 07:51:35 -0700

    ignore next

--- components/Background-img.js
+++ components/Background-img.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Styles from "../styles/Home.module.css"
 
+
 const BackgroundImg = () => {
   return (
     <div className={Styles.BackgroundImg}>
         <div className={Styles.BackgroundImgText}>
+       
            <div className={Styles.BackgroundImgTextH2}> Life is short, make it sweet</div>
             </div>
     </div>
   )
 }
 
-export default BackgroundImg
+export default BackgroundImg  
